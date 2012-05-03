c722cac558f751f3b47bdfa3b200997c3c386be6
usagi <usagi@WonderRabbitProject.net>
2012-05-03 15:37:09 +0900

    入力イベントをハンドるテスト実装

--- Labyrinthian.cxx
+++ Labyrinthian.cxx
@@ -1,5 +1,8 @@
+#include <iostream>
 #include <string>
 #include <algorithm>
+
+#include <boost/lexical_cast.hpp>
 
 #include "ppapi/cpp/instance.h"
 #include "ppapi/cpp/module.h"
@@ -8,19 +11,41 @@
 //#include "ppapi/cpp/rect.h"
 //#include "ppapi/cpp/size.h"
 #include "ppapi/cpp/var.h"
+#include "ppapi/cpp/input_event.h"
 
 namespace wrp { 
   struct Labyrinthian_instance
     : public pp::Instance
   {
-    explicit Labyrinthian_instance(PP_Instance a): pp::Instance(a){}
+    explicit Labyrinthian_instance(PP_Instance a): pp::Instance(a){
+      RequestInputEvents(
+        PP_INPUTEVENT_CLASS_MOUSE |
+        PP_INPUTEVENT_CLASS_WHEEL
+      );
+      RequestFilteringInputEvents(
+        PP_INPUTEVENT_CLASS_KEYBOARD
+      );
+    }
     virtual ~Labyrinthian_instance(){}
     virtual void HandleMessage(const pp::Var& a){
       if(!a.is_string())
         return;
       auto v = a.AsString();
       std::reverse(v.begin(),v.end());
+      std::cerr<<v<<"\n";
       PostMessage(pp::Var(v));
+    }
+    virtual bool HandleInputEvent(const pp::InputEvent& event){
+      PostMessage(pp::Var("HandleInputEvent"));
+      std::cerr << "HandleInputEvent\n";
+      auto m = std::string(
+          "Test: input / handle input event\n"
+          "  type: "
+          );
+      m += boost::lexical_cast<std::string>(event.GetType()) + "\n"
+        "  time: " + boost::lexical_cast<std::string>(event.GetTimeStamp());
+      PostMessage(m);
+      return pp::Instance::HandleInputEvent(event);
     }
     virtual void DidChangeView(const pp::View& view){
       PostMessage("ToDo: resize view / did change view(v)");
